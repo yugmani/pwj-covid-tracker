8d4e59523b5f4a83f056afc86f137d38b2bf54b3
Himalaya <yugmani@yahoo.com>
2020-07-04 13:42:02 -0700

    line-chart added

--- script.js
+++ script.js
@@ -5,13 +5,15 @@ var map;
 
 window.onload = () =>{
     getCountryData();
-    
+    // buildChart();
+    getHistoricalData();
 }
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom:3
+    center: {lat: 39.8283, lng: -98.5795},
+    zoom:3,
+    styles: mapStyle
   });
 
 
@@ -46,24 +48,24 @@ const showDataOnMap = (data)=>{
           center: countryCenter,
           radius: country.casesPerOneMillion * 15
         });
-// style="${country.countryInfo.flag}"
-/* <img src="${country.countryInfo.flag}"></img> */
+
+        // style="background-image:url(${country.countryInfo.flag});"
         var html = `
                 <div class="info-container">
-                    <div class="country-flag" style="background-image:url(${country.countryInfo.flag});"> 
-                    
+                    <div class="country-flag" > 
+                        <img src="${country.countryInfo.flag}"></img>
                     </div>
                     <div class="info-name">
                         ${country.country}
                     </div>
                     <div class="info-confirmed">
-                    Total: ${country.cases}
+                        Total: ${country.cases}
                     </div>
                     <div class="info-recovered">
-                    Recovered: ${country.recovered}
+                        Recovered: ${country.recovered}
                     </div>
                     <div class="info-deaths">
-                    Deaths: ${country.deaths}
+                        Deaths: ${country.deaths}
                     </div>
                 </div>`
         var infoWindow = new google.maps.InfoWindow({
@@ -87,12 +89,89 @@ const showDataInTable = (data) =>{
     var html = "";
     data.forEach((country) => {
         html += `
-            <tr>
-                <td>${country.country}</td>
-                <td>${country.cases}</td>
-                <td>${country.recovered}</td>
-                <td>${country.deaths}</td>
-            </tr>`
+                <tr>
+                    <td>${country.country}</td>
+                    <td>${country.cases}</td>
+                    <td>${country.recovered}</td>
+                    <td>${country.deaths}</td>
+                </tr>`
     })
     document.getElementById("table-data").innerHTML = html;
+}
+
+
+const getHistoricalData = ()=>{
+    fetch("https://disease.sh/v3/covid-19/historical/all?lastdays=90")
+    .then((response)=>{
+        // console.log(response);
+        return response.json();
+    }).then((data)=>{
+        // console.log(data);
+        let chartData = buildChartData(data);
+        buildChart(chartData);
+    })
+}
+
+const buildChartData = (data)=>{
+    let chartData = [];
+    for(let date in data.cases){
+        let newDataPoint = {
+            x: date,
+            y: data.cases[date]
+        }
+        chartData.push(newDataPoint);
+    }
+    // console.log(chartData);
+    return chartData;
+}
+
+
+const buildChart = (chartData)=>{
+    var timeFormat = 'MM/DD/YY';
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            // labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'Total Cases',
+                // backgroundColor: '#1d2c4d',
+                borderColor: '#00CED1',
+                data: chartData
+            }]
+        },
+
+        // Configuration options go here
+        options: {
+            tooltips: {
+                mode: 'index',
+                intersect: false
+            },
+            scales: {
+                xAxes: [{
+                    type: "time",
+                    time: {
+                        format: timeFormat,
+                        tooltipFormat: "ll"
+                    }, 
+                    scaleLable: {
+                        display: true,
+                        lableString: 'Date'
+                    }
+
+                }],
+                yAxes: [{
+                    ticks: {
+                        // Include a dollar sign in the ticks
+                        callback: function(value, index, values) {
+                            return numeral(value).format('0, 0');
+                        }
+                    }
+                }]
+            }
+        }
+    });
 }
